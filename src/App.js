5dc13a5d5bf2cad215db049b65c3301f49682249
Hamza Gobir <hgobir@googlemail.com>
2020-10-03 18:29:00 +0100

    commented out

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import uuid from 'uuid';
+// import uuid from 'uuid';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import TodoInput from './components/TodoInput'
-import TodoItem from './components/TodoItem'
+// import TodoItem from './components/TodoItem'
 import TodoList from './components/TodoList'
 
 // random
