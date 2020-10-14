cb13d8a7d114e67f2d9d35acd55954729ca17c05
Hamza Gobir <hgobir@googlemail.com>
2020-10-14 19:55:30 +0100

    random commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,59 @@
-import React from 'react';
-// import uuid from 'uuid';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import TodoInput from './components/TodoInput'
-// import TodoItem from './components/TodoItem'
-import TodoList from './components/TodoList'
+import React, {Component} from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TodoInput from './components/TodoInput';
+// import TodoItem from './components/TodoItem';
+import TodoList from './components/TodoList';
 
 // random
-function App() {
+class App extends Component {
+
+  state = {
+    items: [{ id: 1, title: 'wake up' }, { id: 2, title: 'make breakfast' }],
+    id:uuidv4(),
+    item: '',
+    editItem:false
+  };
+
+  handleChange = (e) => {
+    console.log('handle change');
+  }
+
+  handleSubmit = (e) => {
+    console.log('handle submit');
+  }
+
+  clearList = () => {
+    console.log('clea list method');
+  }
+
+  handleDelete = (id) => {
+    console.log(`handle delete ${id}`);
+  }
+
+  handleEdit = (id) => {
+    console.log(`handle edit ${id}`);
+  }
+
+  render() {
+    console.log(this.state);
   return (
-    <div className="App">
-      <TodoInput />
-      <TodoList />
+    
+    <div className="container">
+      <div className="row">
+        <div className="col-10 mx-auto col-md-8 mt-5">
+          <h3 className="text-capitalize text-center">
+            todo input
+          </h3>
+
+            <TodoInput item={this.state.item} />
+            <TodoList />
+        </div>
+      </div>
+
     </div>
-  );
+    );
+  }
 }
 
 export default App;
