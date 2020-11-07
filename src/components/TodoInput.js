8e3ec6165fd13bb752d5dc94303f7e8d30f00eaf
Hamza Gobir <hgobir@googlemail.com>
2020-11-07 17:17:33 +0000

    added some bootstrap styling

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,10 +2,29 @@ import React, { Component } from 'react'
 
 export default class TodoInput extends Component {
     render() {
+
+        const {item, handleChange, handleSubmit, editItem} = this.props
         return (
-            <h1>
-                hello from todo input
-            </h1>
+            <div className="card card-body mt-3">
+                <form onSubmit={handleSubmit}>
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                            <div className="input-group-text bg-primary text-white">
+                                <i className="fas fa-book"/>
+                            </div>
+                        </div>
+                        <input 
+                            type="text" 
+                            className="form-control text-capitalize" 
+                            placeholder="add todo item" 
+                            value={item} 
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-block btn-primary text-capitalize mt-3">add item</button>
+                </form>
+            
+            </div>
         )
     }
 }
